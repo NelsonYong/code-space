9f65e527d2ca3a202e87f38b028e5aeb1ef3fa6c
YongGit <1013588891@qq.com>
2023-05-07 15:16:01 +0800

    feat: search

--- docs/.vitepress/routers/algorithm.ts
+++ docs/.vitepress/routers/algorithm.ts
@@ -11,9 +11,14 @@ const algorithm_route: DefaultTheme.Sidebar = [{
         {
           text: "查找-概览",
           link: "/algorithm/search/"
-        }, {
+        },
+        {
           text: "二维数组的查找",
           link: "/algorithm/search/two-dimension-array"
+        },
+        {
+          text: "旋转数组的最小数字",
+          link: "/algorithm/search/rotate-array-min"
         }
       ]
     },
